2edfde5c607981b275c6bc0a87a7db640a9b5f4e
orionokc <cohenokc@gmail.com>
2023-09-24 22:23:48 -0400

    Ran 1000 permutations on the data
    Ran 1000 permutations on the data and counted the number of times the average variance of the stock price of the representative's trades was higher in the permutation than in the actual data. Saved the results into permutations.csv

---
+++ permutations.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+from dateutil.relativedelta import relativedelta
+
+returns = pd.read_csv('returns.csv')
+returns.index = returns.Date.astype('datetime64[ns]')
+returns.drop('Date', axis=1, inplace=True)
+
+def permutation(df):
+    permi=df.copy()
+    df2=df.drop(['Representative','Party','House'],axis=1)
+    columns=['ReportDate','TransactionDate','Ticker','Transaction','Amount','Range','Sentiment','Price','Low_End','High_End','Adjusted_Low_End','Adjusted_High_End','Running_Portfolio_Low_End','Running_Portfolio_High_End','Est_Portfolio_Value','Shares_High_End','Shares_Low_End']
+    permi[columns]=pd.DataFrame(np.random.permutation(df2))
+    return permi
+
+def npermis(df, i):
+    qr=[]
+    for n in range(i):
+        qr.append(permutation(df))
+    return qr
+
+def calculate_variance(row):
+    ticker = row['Ticker']
+    transaction_date = row['TransactionDate']
+    
+    # Find the index in returns corresponding to the TransactionDate
+    index = transaction_date
+    
+    # Calculate variance for the next 3 months (90 days)
+    end_index = transaction_date + relativedelta(months=3)
+    variance = np.var(returns[ticker][index:end_index])
+    
+    return variance
